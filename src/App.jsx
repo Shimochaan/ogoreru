2c7a8e5a1c904583196dbbaf7178b5b6fe7c8f2b
Shimochaan <225631722+Shimochaan@users.noreply.github.com>
2026-05-17 15:48:32 +0900

    スタートボタンを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center flex-col gap-6">
       <h1 className="text-white text-5xl font-bold tracking-widest">
         オゴレル
       </h1>
+      <button className=" bg-white text-black px-8 py-3 rounded-lg text-lg font-medium">スタート</button>
     </div>
   )
 }
